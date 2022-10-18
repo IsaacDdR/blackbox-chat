c0a5c684e0f7bef8f58594864123fb54c5fe7ac5
Isaac Diaz <isaac@bidout.app>
2022-10-17 22:04:22 -0500

    added 0.0.0.0 endpoint into the listener with error param

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import { PORT } from "./config";
 
 connectDB();
 const server = http.createServer(app);
-const httpServer = server.listen(PORT, "0.0.0.0");
-console.log("Server on http://localhost:", PORT);
+const httpServer = server.listen(PORT, "0.0.0.0", (err) => {
+  if (err) throw err;
+  console.log("Server ready");
+});
 
 const io = new WebSocketServer(httpServer);
 
